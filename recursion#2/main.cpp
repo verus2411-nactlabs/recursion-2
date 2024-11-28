bba8cfe08e97924ca6a048aeacc6499c5be86a89
verus2411-nactlabs <lehonglong2411@gmail.com>
2024-11-29 00:37:03 +0700

    Update main.cpp

--- recursion#2/main.cpp
+++ recursion#2/main.cpp
@@ -9,8 +9,13 @@
 
 using namespace std;
 
+int T;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, VietNam" << endl;
+    for( int test_no = 1; test_no <= T; test_no++ ){
+    
+    }
     return 0;
 }
